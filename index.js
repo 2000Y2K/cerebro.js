4ba47ae8f11151552a554a76cdd1ecb872727724
Juan <juanespinocha@gmail.com>
2021-07-09 19:26:32 -0300

    primer highlight y touch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r127/three.module.js';
-import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
-import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.119.1/build/three.module.js';
+import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.119.1/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.119.1/examples/jsm/loaders/GLTFLoader.js';
 //import {GUI} from './dat.gui.module.js';
 
 
@@ -15,6 +15,7 @@ function main() {
   let cerebros;
   let cerebro;
   const esferas = [];
+  const highlights = [];
   const nombreEsferas =["a","b","c","d","e","f"];
   const tempV = new THREE.Vector3();
   const raycaster = new THREE.Raycaster();
@@ -25,16 +26,54 @@ function main() {
   const mouse = new THREE.Vector2(1,1);
   document.body.appendChild(renderer.domElement);
 
+
+
+  function cargarHighlight(nombre)
+  {
+    gltfLoader.load('resources/Brain '+nombre+'.gltf' , (gltf) => {
+      const root = gltf.scene;
+      const highlight = root.getObjectByName("BrainSolid_Light_"+nombre);
+      highlight.material.emissiveIntensity = 3;
+      highlight.position.set(0,0,0);
+      highlight.rotation.set(0,0,0);
+      highlight.name = "highlight_"+nombre;
+      cerebro.add(highlight);
+      highlight.visible = false;
+      highlights.push(highlight);
+    });
+  }
+
+  function cargarEsferas()
+  {
+          gltfLoader.load('resources/IPoint A.gltf', (gltf) => {
+        const root = gltf.scene;
+        const esfera = root.getObjectByName("IPoint_A");
+        esferas.push(hacerEsfera(esfera,0.250,4.840,-0.250,"a"));
+        esferas.push(hacerEsfera(esfera,-0.950,6.820,-0.130,"b"));
+        esferas.push(hacerEsfera(esfera,-1.440,3.210,-2.780,"c"));
+        esferas.push(hacerEsfera(esfera,0.990,5.370,-2.260,"d"));
+        esferas.push(hacerEsfera(esfera,-3.770,5.370,-2.390,"e"))
+        esferas.push(hacerEsfera(esfera,-1.550,5.910,-5.160,"f"));
+    });
+  }
+
+  function cargarEtiquetas()
+  {
+    cerebro.add(hacerEtiqueta(1.650,2.190,0.710,"atrio"));
+    cerebro.add(hacerEtiqueta(-0.070,-0.320,2.370,"arium"));
+    cerebro.add(hacerEtiqueta(2.530,0.180,-0.170,"cemdoe"));
+    cerebro.add(hacerEtiqueta(-0.140,-2.730,-0.710,"afinis"));
+    cerebro.add(hacerEtiqueta(-2.510,0.030,-0.170,"allegra"));
+    cerebro.add(hacerEtiqueta(0.4,2.260,-1.690,"fundacion"));
+
+  }
+
   function hacerEtiqueta(x,y,z,texto)
   {
 
     const map = new THREE.TextureLoader().load( 'resources/'+texto+'.png');
     const labelMaterial = new THREE.SpriteMaterial( { map: map} );
     const label = new THREE.Sprite(labelMaterial);
-    // label.scale.set(1.5,1.5,1.5);
-    //label.anisotropy = renderer.capabilities.getMaxAnisotropy();       
-    //label.minFilter = THREE.LinearFilter;
-    //label.minFilter = THREE.NearestFilter;
     label.renderOrder = 3;
     label.position.x = x;
     label.position.y = y;
@@ -57,28 +96,65 @@ function main() {
   ///// MOVIMIENTO
   const controls = new OrbitControls(camera, canvas);
   controls.target.set(0, -5, 0);
-  controls.autoRotate = true;;
+  controls.autoRotate = false;
   controls.autoRotateSpeed = 1;
   controls.enableDamping = true;
   controls.enableZoom = true;
-  controls.minDistance = 8;
+  //controls.minDistance = 8;
   controls.maxDistance = 10;
-  controls.enablePan = false;
+  controls.enablePan = true;
   controls.update();
   /////
 
   ///// MOUSE 
    
-  window.addEventListener("mousemove", (event) =>
-  {
-    mouse.x = (event.clientX /window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY/window.innerHeight)  * 2 + 1 ;
-   // console.log("x ",mouse.x," y ",mouse.y);
+   window.addEventListener("mousemove", setPickPosition);
+   window.addEventListener('mouseout', clearPickPosition);
+  window.addEventListener('mouseleave', clearPickPosition);
+ // (event) =>
+  // {
+  //   mouse.x = (event.clientX /window.innerWidth) * 2 - 1;
+  //   mouse.y = -(event.clientY/window.innerHeight)  * 2 + 1 ;
+  //  // console.log("x ",mouse.x," y ",mouse.y);
+  // })
+  
+
+  window.addEventListener('touchstart', (event) => {
+    // prevent the window from scrolling
+    event.preventDefault();
+    setPickPosition(event.touches[0]);
+  }, {passive: false});
+   
+  window.addEventListener('touchmove', (event) => {
+    setPickPosition(event.touches[0]);
+  });
+   
+  window.addEventListener('touchend', clearPickPosition);
+
+
+  function getCanvasRelativePosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: (event.clientX - rect.left) * canvas.width  / rect.width,
+      y: (event.clientY - rect.top ) * canvas.height / rect.height,
+    };
   }
-  )
-
-
-
+   
+  function setPickPosition(event) {
+    const pos = getCanvasRelativePosition(event);
+    mouse.x = (pos.x / canvas.width ) *  2 - 1;
+    mouse.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
+  }
+   
+  function clearPickPosition() {
+    // unlike the mouse which always has a position
+    // if the user stops touching the screen we want
+    // to stop picking. For now we just pick a value
+    // unlikely to pick something
+    mouse.x = -100000;
+    mouse.y = -100000;
+  }
+  ////
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color('#00485B'); // 00485B
@@ -97,16 +173,16 @@ function main() {
     const intensity = 0.15;
     const light2 = new THREE.PointLight(color, intensity);
     const light3 = new THREE.PointLight(color, intensity);
-    const light6 = new THREE.PointLight(color, intensity);
-    const light4 = new THREE.PointLight(color, intensity);
+    // const light6 = new THREE.PointLight(color, intensity);
+    // const light4 = new THREE.PointLight(color, intensity);
     const light1 = new THREE.DirectionalLight(color, 0.5);
-    const light5 = new THREE.DirectionalLight(color, 0.5);
+    // const light5 = new THREE.DirectionalLight(color, 0.5);
     light1.position.set(1.6,9,0);
     light2.position.set(1,1,10);
     light3.position.set(-3.7,1,0); // -1.1 0.2 -5
-    light4.position.set(3.7,1,0);
-   light5.position.set(-9.820,9,0);
-   light6.position.set(-1.1,0.2,-5);
+  //   light4.position.set(3.7,1,0);
+  //  light5.position.set(-9.820,9,0);
+  //  light6.position.set(-1.1,0.2,-5);
     scene.add(light1);
     scene.add(light2);
     scene.add(light3);
@@ -144,17 +220,17 @@ function main() {
     camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
   }
 
-  // function dumpObject(obj, lines = [], isLast = true, prefix = '') {
-  //   const localPrefix = isLast ? '└─' : '├─';
-  //   lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
-  //   const newPrefix = prefix + (isLast ? '  ' : '│ ');
-  //   const lastNdx = obj.children.length - 1;
-  //   obj.children.forEach((child, ndx) => {
-  //     const isLast = ndx === lastNdx;
-  //     dumpObject(child, lines, isLast, newPrefix);
-  //   });
-  //   return lines;
-  // }
+  function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+    const localPrefix = isLast ? '└─' : '├─';
+    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+    const newPrefix = prefix + (isLast ? '  ' : '│ ');
+    const lastNdx = obj.children.length - 1;
+    obj.children.forEach((child, ndx) => {
+      const isLast = ndx === lastNdx;
+      dumpObject(child, lines, isLast, newPrefix);
+    });
+    return lines;
+  }
 
   function hacerEsfera(mesh,x,y,z,nombre)
   {
@@ -164,7 +240,7 @@ function main() {
     esfera.material.depthWrite = true;
     esfera.name = nombre
 
-    console.log("posicion esfera",x,y,z);
+    //console.log("posicion esfera",x,y,z);
 
     scene.add(esfera);
 
@@ -182,23 +258,14 @@ function main() {
       scene.add(cerebro);
       scene.add(wires);
 
-      gltfLoader.load('resources/IPoint A.gltf', (gltf) => {
-        const root = gltf.scene;
-        const esfera = root.getObjectByName("IPoint_A");
-        esferas.push(hacerEsfera(esfera,0.250,4.840,-0.250,"a"));
-        esferas.push(hacerEsfera(esfera,-0.950,6.820,-0.130,"b"));
-        esferas.push(hacerEsfera(esfera,-1.440,3.210,-2.780,"c"));
-        esferas.push(hacerEsfera(esfera,0.990,5.370,-2.260,"d"));
-        esferas.push(hacerEsfera(esfera,-3.770,5.370,-2.390,"e"))
-        esferas.push(hacerEsfera(esfera,-1.550,5.910,-5.160,"f"));
-    });
+      cargarEsferas();
+      cargarEtiquetas();
+      cargarHighlight("A");
+      cargarHighlight("B");
+      cargarHighlight("C");
+      // cargarHighlight("D");
+      cargarHighlight("E");
     
-      cerebro.add(hacerEtiqueta(1.650,2.190,0.710,"atrio"));
-      cerebro.add(hacerEtiqueta(-0.070,-0.320,2.370,"arium"));
-      cerebro.add(hacerEtiqueta(2.530,0.180,-0.170,"cemdoe"));
-      cerebro.add(hacerEtiqueta(-0.140,-2.730,-0.710,"afinis"));
-      cerebro.add(hacerEtiqueta(-2.510,0.030,-0.170,"allegra"));
-      cerebro.add(hacerEtiqueta(0.4,2.260,-1.690,"fundacion"));
 
 
     //cerebro.material.transparent = false;
@@ -241,9 +308,6 @@ function main() {
     return needResize;
   } 
 
-
-  
-  console.log("escene",scene.children)
   function render(time) {
     time *= 0.001;
 
@@ -255,14 +319,19 @@ function main() {
 
     if (intersecados.length)
     {
-      //console.log("intersecadoasd",intersecados);
       for (let intersecado of intersecados)
       {
         if(nombreEsferas.includes(intersecado.object.name) && intersecados[0].object == intersecado.object)
         {
            // console.log("si",intersecado.object.name);
-            intersecado.object.visible = false;
-            setTimeout(() => intersecado.object.visible = true, 500);
+            //intersecado.object.visible = false; 
+            //setTimeout(() => intersecado.object.visible = true, 500);
+            switch(intersecado.object.name)
+            {
+              case 'a':
+                highlights[0].visible = true;
+                setTimeout(() => highlights[0].visible = false,5000);
+            }
         }
       }
     }
@@ -277,7 +346,7 @@ function main() {
         controls.minDistance = 5.5 + canvas.clientWidth*0.015;
       }
       else {
-        controls.minDistance = 8;
+        //controls.minDistance = 8;
       }
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
@@ -293,56 +362,3 @@ function main() {
 }
 
 main();
-
-
-
-
-
-
-
-    // matrizNormalizada.getNormalMatrix(camera.matrixWorldInverse);
-    // camera.getWorldPosition(posicionCamara);
-
-    // for (const esfera of esferas){
-
-    //   const posicion = new THREE.Vector3();
-    //   esfera.getWorldPosition(posicion);
-    //   //console.log(posicion);
-    //   tempV.copy(posicion);
-    //   tempV.applyMatrix3(matrizNormalizada).normalize();
-
-    // camaraAPunto.copy(posicion);
-    //  camaraAPunto.applyMatrix4(camera.matrixWorldInverse).normalize();
-
-    //  const productoPunto = tempV.dot(camaraAPunto);
-    //  const distancia = camaraAPunto.manhattanLength();
-
-
-
-    //  esfera.updateWorldMatrix(true,false);
-    //  esfera.getWorldPosition(tempV);
-
-     // tempV.project(camera);
-
-      // raycaster.setFromCamera(tempV,camera);
-      // const objetosIntersecados = raycaster.intersectObjects(scene);
-      // //setTimeout(() => {console.log(objetosIntersecados.length,objetosIntersecados);}, 5000)
-      // //const mostrar = objetosIntersecados.length && esfera === objetosIntersecados[0].object;
-
-
-      // if( productoPunto < -0.2 || productoPunto < 0.4  )//|| distancia > 1.6)
-      // {
-      //   //console.log("hola!");
-      //   esfera.visible = true;
-      //   setTimeout(() => {console.log("distancia",distancia,"producto punto",productoPunto)}, 5000);
-      // }
-      // else {
-
-      //   //console.log("malo");
-      //   //console.log("este entro",productoPunto)
-
-      //   esfera.visible = false;
-        
-      //   //esfera.geometry.scale = 1.5
-      // }
-    
